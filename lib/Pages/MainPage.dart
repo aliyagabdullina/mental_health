ccb24a1170071002e91c53f09a3d2dc57656a783
aliyagabdullina <gabdullina.am29@gmail.com>
2023-02-21 18:40:06 +0300

    Divided into classes, code refactoring, started main page

---
+++ lib/Pages/MainPage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color(0xFFB6B6B6);
+Color whiteTextColor = Color(0xFFFFFFFF);
+
+class MainPageState extends StatefulWidget {
+  MainPageState({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPageState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: backgroundColor,
+      body: Center(
+        child: Container(
+          width: 350,
+          //color: backgroundColor,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              Text('Занятия для тебя'),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 253,
+                height: 253,
+                child: Placeholder(),
+              ),
+              SizedBox(height: 30),
+              Text('Предложения дня'),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 350,
+                height: 210,
+                child: Placeholder(),
+              ),
+              SizedBox(height: 30),
+              Text('Новости'),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 350,
+                height: 210,
+                child: Placeholder(),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 350,
+                height: 210,
+                child: Placeholder(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Pages/SettingsPage.dart
+++ lib/Pages/SettingsPage.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:Mental_Health/Pages/MainPage.dart';
-import 'package:Mental_Health/Pages/SettingsPage.dart';
-
-void main() {
-  runApp(MyApp());
-}
 
 Color backgroundColor = Color(0xFFB6B6B6);
 Color whiteTextColor = Color(0xFFFFFFFF);
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Settings App',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        backgroundColor: Colors.blueAccent,
-      ),
-      home: MainPageState(),
-    );
-  }
-}
-
 
 class SettingsPageState extends StatefulWidget {
   SettingsPageState({Key? key}) : super(key: key);
