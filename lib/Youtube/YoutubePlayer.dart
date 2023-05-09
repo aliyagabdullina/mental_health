cb8df01fe43470ccf1eda48f8a9a27c6170a7656
aliyagabdullina <gabdullina.am29@gmail.com>
2023-05-09 23:12:38 +0300

    text

--- lib/Youtube/YoutubePlayer.dart
+++ lib/Youtube/YoutubePlayer.dart
@@ -1,50 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
   final String videoUrl;
 
-  const VideoPlayerWidget({Key? key, required this.videoUrl}) : super(key: key);
+  VideoPlayerWidget({required this.videoUrl});
 
   @override
-  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
 }
 
 class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late YoutubePlayerController _youtubeController;
+  late VideoPlayerController _videoPlayerController;
+  late Future<void> _initializeVideoPlayerFuture;
 
   @override
   void initState() {
+    _videoPlayerController = VideoPlayerController.network(widget.videoUrl);
+    _initializeVideoPlayerFuture = _videoPlayerController.initialize();
     super.initState();
-    final videoID = YoutubePlayer.convertUrlToId(widget.videoUrl);
-    _youtubeController = YoutubePlayerController(
-        initialVideoId: videoID!,
-        flags: const YoutubePlayerFlags(autoPlay: false, isLive: false));
-  }
-
-  @override
-  void deactivate() {
-    // Pauses video while navigating to next page.
-    _youtubeController.pause();
-    super.deactivate();
   }
 
   @override
   void dispose() {
-    _youtubeController.dispose();
+    _videoPlayerController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          YoutubePlayer(
-            controller: _youtubeController,
-            showVideoProgressIndicator: true,
-          )
-        ],
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _videoPlayerController.value.aspectRatio,
+              child: VideoPlayer(_videoPlayerController),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_videoPlayerController.value.isPlaying) {
+              _videoPlayerController.pause();
+            } else {
+              _videoPlayerController.play();
+            }
+          });
+        },
+        child: Icon(
+          _videoPlayerController.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
       ),
     );
   }
